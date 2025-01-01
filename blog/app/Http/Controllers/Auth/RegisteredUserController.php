d818f207a602fbd5778d9614adebdba077168f4c
mushahadur <mushahadurrahamankhanpulock@gmail.com>
2025-01-02 01:35:49 +0600

    uu

--- blog/app/Http/Controllers/Auth/RegisteredUserController.php
+++ blog/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,7 +41,7 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-
+dd($user);
         event(new Registered($user));
 
         Auth::login($user);
